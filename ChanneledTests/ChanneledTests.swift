7bb52b106f26261ab1532a9d82ff90dd42c4d76c
Darrenc81 <darrenlcox@icloud.com>
2026-02-12 15:46:42 +0000

    Initial Commit

---
+++ ChanneledTests/ChanneledTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChanneledTests.swift
+//  ChanneledTests
+//
+//  Created by Darren on 12/02/2026.
+//
+
+import Testing
+@testable import Channeled
+
+struct ChanneledTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
